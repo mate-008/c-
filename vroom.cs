4bfdbe0885bd3f528752d08fab6e6cc72dee6a8f
kkeivn <mate.petrovics2008@gmail.com>
2025-10-06 14:05:17 +0200

    Add files via upload

---
+++ vroom.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace vroom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Teljes elérési út
+            string filePath = @"C:\Users\user\Downloads\auto.txt";
+
+            // Fájl soronkénti beolvasása
+            List<string> sorok = new List<string>(File.ReadAllLines(filePath));
+
+            // Teszt kiíratása
+            foreach (var sor in sorok)
+            {
+                Console.WriteLine(sor);
+            }
+        }
+    }
+}
