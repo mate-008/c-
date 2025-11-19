e1a0357a1b41801ef87687bafe7e9e14fd89be80
kkeivn <mate.petrovics2008@gmail.com>
2025-11-19 10:57:01 +0100

    Add files via upload

---
+++ konyvkiado.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyves
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fajl = @"C:\Premo\Asztali_alkalmazások_fejlesztése\Konyv\kiadas.txt";
+
+
+            List<List<string>> adatok = new List<List<string>>();
+
+            using (StreamReader r = new StreamReader(fajl))
+            {
+                string sor;
+                while ((sor = r.ReadLine()) != null)
+                {
+                    // év;negyed;eredet;leiras;peldany
+                    string[] v = sor.Split(';');
+
+                    // Konvertálás beolvasáskor
+                    v[0] = int.Parse(v[0]).ToString(); // év
+                    v[1] = int.Parse(v[1]).ToString(); // negyedév
+                    v[4] = int.Parse(v[4]).ToString(); // példányszám
+
+                    adatok.Add(new List<string>(v));
+                }
+            }
+
+            Console.WriteLine("Beolvasva.\n");
+
+
+            // 2. feladat
+            Console.WriteLine("2. feladat:");
+            Console.Write("Írja be a szerző nevét: ");
+            string szerzo = Console.ReadLine();
+
+            int db = 0;
+            foreach (var sor in adatok)
+            {
+                string leiras = sor[3];
+                if (leiras.Contains(szerzo))
+                    db++;
+            }
+
+            if (db == 0)
+                Console.WriteLine("Nem adtak ki");
+            else
+                Console.WriteLine($"A szerzőtől {db} mű szerepel.\n");
+
+
+            // 3. feladat
+            Console.WriteLine("3. feladat:");
+
+            List<int> peldanyok = new List<int>();
+            foreach (var s in adatok)
+                peldanyok.Add(int.Parse(s[4]));
+
+            int maxPel = peldanyok.Max();
+            int darab = peldanyok.Count(x => x == maxPel);
+
+            Console.WriteLine($"A legnagyobb példányszám: {maxPel}");
+            Console.WriteLine($"Ennyi könyvnél fordult elő: {darab}");
+        }
+    }
+}
